e02b15f7adc41c26ecdbcb272ae80e3bbe54e337
0xBakunawa <trong@autonomous.nyc>
2018-10-03 17:05:04 +0700

    [WIP] port bee to gin

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	_ "github.com/ninjadotorg/uncommons-service/routers"
-
-	"github.com/astaxie/beego"
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
-	beego.Run()
+
 }
--- models/app_proposal_author.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/ninjadotorg/uncommons/common"
-
-// AppProposalAuthor ...
-type AppProposalAuthor struct {
-	ID       int64
-	AuthorID common.Address
-}
