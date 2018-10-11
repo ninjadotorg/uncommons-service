19a0f28f1615c80bb98d143285ef6233560e4515
0xBakunawa <trong@autonomous.nyc>
2018-10-11 16:54:59 +0700

    Add go-ethereum

--- controllers/user.go
+++ controllers/user.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/ninjadotorg/uncommons-service/utils"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,12 +13,19 @@ type UserController struct{}
 
 // SignUp ...
 func (u UserController) SignUp(c *gin.Context) {
-	uncommonsWalletAddres := c.DefaultPostForm("uncommons_address", "")
+	uncommonsWalletAddress := c.DefaultPostForm("uncommons_address", "")
 	uncommonsSignedHash := c.DefaultPostForm("uncommons_signed_hash", "")
 	uncommonsText := c.DefaultPostForm("uncommons_text", "")
 
-	if len(uncommonsWalletAddres) == 0 || len(uncommonsSignedHash) == 0 || len(uncommonsText) == 0 {
+	if len(uncommonsWalletAddress) == 0 || len(uncommonsSignedHash) == 0 || len(uncommonsText) == 0 {
 		resp := JSONResponse{0, "Missing parameters", nil}
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+
+	res := utils.VerifySig(uncommonsWalletAddress, uncommonsSignedHash, []byte(uncommonsText))
+	if !res {
+		resp := JSONResponse{0, "Invalid parameters", nil}
 		c.JSON(http.StatusOK, resp)
 		return
 	}
---
+++ utils/ethereum.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// VerifySig ...
+func VerifySig(from, sigHex string, msg []byte) bool {
+	fromAddr := common.HexToAddress(from)
+
+	sig := hexutil.MustDecode(sigHex)
+	// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
+	if sig[64] != 27 && sig[64] != 28 {
+		return false
+	}
+	sig[64] -= 27
+
+	pubKey, err := crypto.SigToPub(signHash(msg), sig)
+	if err != nil {
+		return false
+	}
+
+	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+
+	return fromAddr == recoveredAddr
+}
+
+// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L404
+// signHash is a helper function that calculates a hash for the given message that can be
+// safely used to calculate a signature from.
+//
+// The hash is calculated as
+//   keccak256("\x19Ethereum Signed Message:\n"${message length}${message}).
+//
+// This gives context to the signed message and prevents signing of transactions.
+func signHash(data []byte) []byte {
+	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
+	return crypto.Keccak256([]byte(msg))
+}
